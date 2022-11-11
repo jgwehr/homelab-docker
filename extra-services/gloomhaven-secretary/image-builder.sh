a0d58f547e7f31164aca0346a37e26274a675680
jgwehr <dm.nota@gmail.com>
2022-11-11 11:06:21 -0600

    Don't talk to me and my variables ever again
    hardcoding is for suckers

--- extra-services/gloomhaven-secretary/image-builder.sh
+++ extra-services/gloomhaven-secretary/image-builder.sh
@@ -1,6 +1,10 @@
-
 # $1 = version tag
 varRepo=snoopeppers
+varClient=gloomhaven-secretary
+varServer=gloomhaven-secretary-server
+
+varClientGit=https://github.com/Lurkars/gloomhavensecretary.git
+varServerGit=https://github.com/Lurkars/ghs-server.git
 
 #require a tag
 if [[ -z $1 ]]
@@ -10,23 +14,23 @@ then
 fi
 
 # check if the tag has already been pulled or not
-varImageClient=$(docker image ls -q $varRepo/gloomhaven-secretary:$1)
-varImageServer=$(docker image ls -q $varRepo/gloomhaven-secretary-server:$1)
+varImageClient=$(docker image ls -q $varRepo/$varClient:$1)
+varImageServer=$(docker image ls -q $varRepo/$varServer:$1)
 
 #skip if the image:tag already exists
 if [[ -z $varImageClient ]]
 then
     # Pull the image. Build the image. Tag it to repo/container:tag
     echo Pulling and Building Client image for $1 ... \(longest step\)
-    docker build --rm -q --tag $varRepo/gloomhaven-secretary:$1 https://github.com/Lurkars/gloomhavensecretary.git#$1
+    docker build --rm -q --tag $varRepo/$varClient:$1 $varClientGit#$1
 
     # For simplicity sake, we assume we're only building the latest
     echo Committing container image to :latest ...
-    docker container commit $varRepo/gloomhaven-secretary:latest $varRepo/gloomhaven-secretary:latest >/dev/null 2>&1
+    docker container commit $varRepo/$varClient:latest $varRepo/$varClient:latest >/dev/null 2>&1
 
     echo Pushing images to DockerHub...
-    docker image push $varRepo/gloomhaven-secretary:$1 >/dev/null 2>&1
-    docker image push $varRepo/gloomhaven-secretary:$latest >/dev/null 2>&1
+    docker image push $varRepo/$varClient:$1 >/dev/null 2>&1
+    docker image push $varRepo/$varClient:$latest >/dev/null 2>&1
 else
     echo Tag exist already for Client. Skipping.
 fi
@@ -35,18 +39,18 @@ fi
 if [[ -z $varImageServer ]]
 then
     echo Pulling and Building Server image for $1 ... \(longest step\)
-    docker build --rm -q --tag $varRepo/gloomhaven-secretary-server:$1 https://github.com/Lurkars/ghs-server.git#$1
+    docker build --rm -q --tag $varRepo/$varServer:$1 $varServerGit#$1
 
     echo Committing container image to :latest ...
-    docker container commit $varRepo/gloomhaven-secretary-server:latest $varRepo/gloomhaven-secretary-server:latest >/dev/null 2>&1
+    docker container commit $varRepo/$varServer:latest $varRepo/$varServer:latest >/dev/null 2>&1
 
     echo Pushing images to DockerHub...
-    docker image push $varRepo/gloomhaven-secretary-server:$1 >/dev/null 2>&1
-    docker image push $varRepo/gloomhaven-secretary-server:latest >/dev/null 2>&1
+    docker image push $varRepo/$varServer:$1 >/dev/null 2>&1
+    docker image push $varRepo/$varServer:latest >/dev/null 2>&1
 else
     echo Tag exist already for Server. Skipping.
 fi
 
 echo
-docker image ls --filter "reference=$varRepo/gloomhaven-secretary*$1" --filter "reference=$varRepo/gloomhaven-secretary*latest" | grep gloomhaven-secretary
+docker image ls --filter "reference=$varRepo/varClient*$1" --filter "reference=$varRepo/varClient*latest"
 echo
