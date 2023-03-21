fa8ffaa8a2e0c5205e5d8e00f19056b8dca07154
jgwehr <dm.nota@gmail.com>
2023-03-21 16:05:34 -0500

    Backup Cleanup and Pinry
    Try to reduce folder-within-folder. Add Pinry. General readability. Only grab latest Podgrab backup. Scan main zip file for sizes

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -15,7 +15,7 @@ sudo docker exec -t tandoor_db pg_dumpall -U tandoor_user > tandoor_pgdump.sql
 # Docker config backups
 echo Backing up Docker Configs...
 
-mkdir -p $varBackupDir/$varDate/ghs && cp -rpi $varConfigDir/ghs $varBackupDir/$varDate/ghs
+mkdir -p $varBackupDir/$varDate/ghs && cp -rpi $varConfigDir/ghs $varBackupDir/$varDate
 
 mkdir -p $varBackupDir/$varDate/homepage && cp -rpi $varConfigDir/homepage/*.yaml $varBackupDir/$varDate/homepage
 
@@ -28,6 +28,8 @@ mkdir -p $varBackupDir/$varDate/jellyseerr
 cp -rpi $varConfigDir/jellyseerr/db $varBackupDir/$varDate/jellyseerr
 cp -rpi $varConfigDir/jellyseerr/settings.json $varBackupDir/$varDate/jellyseerr
 
+
+# the *arr containers smartly have scheduled backups. So we pull only the most recent to save disk space
 mkdir -p $varBackupDir/$varDate/prowlarr
 varTempProwlarrBackup=$(ls -Art $varConfigDir/prowlarr/Backups/scheduled | tail -n 1)
 cp -rpi $varConfigDir/prowlarr/Backups/scheduled/$varTempProwlarrBackup $varBackupDir/$varDate/prowlarr
@@ -43,26 +45,29 @@ cp -rpi $varConfigDir/sonarr/Backups/scheduled/$varTempSonarrBackup $varBackupDi
 mkdir -p $varBackupDir/$varDate/qbittorrent
 cp -rpi $varConfigDir/qbt/qBittorrent/qBittorrent.conf $varBackupDir/$varDate/qbittorrent
 
-mkdir -p $varBackupDir/$varDate/pihole
-cp -rpi $varConfigDir/pihole $varBackupDir/$varDate/pihole
+mkdir -p $varBackupDir/$varDate/pihole && cp -rpi $varConfigDir/pihole $varBackupDir/$varDate
+
+mkdir -p $varBackupDir/$varDate/pinry
+cp -rpi $varConfigDir/pinry/*.* $varBackupDir/$varDate/pinry
 
 mkdir -p $varBackupDir/$varDate/podgrab
-cp -rpi $varConfigDir/podgrab $varBackupDir/$varDate/podgrab
+cp -rpi $varConfigDir/podgrab/podgrab.db $varBackupDir/$varDate/podgrab
+varTempPodgrabBackup=$(ls -Art $varConfigDir/podgrab/backups | tail -n 1)
+cp -rpi $varConfigDir/podgrab/backups $varBackupDir/$varDate/podgrab
 
-mkdir -p $varBackupDir/$varDate/ripping
-cp -rpi $varConfigDir/ripping $varBackupDir/$varDate/ripping
+mkdir -p $varBackupDir/$varDate/ripping && cp -rpi $varConfigDir/ripping $varBackupDir/$varDate/ripping
 
-mkdir -p $varBackupDir/$varDate/scrutiny
-cp -rpi $varConfigDir/scrutiny $varBackupDir/$varDate/scrutiny
+mkdir -p $varBackupDir/$varDate/scrutiny && cp -rpi $varConfigDir/scrutiny $varBackupDir/$varDate/scrutiny
 
-mkdir -p $varBackupDir/$varDate/unbound
-cp -rpi $varConfigDir/unbound $varBackupDir/$varDate/unbound
+mkdir -p $varBackupDir/$varDate/unbound && cp -rpi $varConfigDir/unbound $varBackupDir/$varDate/unbound
 
 mkdir -p $varBackupDir/$varDate/uptime-kuma
 cp -rpi $varConfigDir/uptime-kuma/kuma.db $varBackupDir/$varDate/uptime-kuma
 
 mkdir -p $varBackupDir/$varDate/wireguard
 cp -rpi $varConfigDir/wireguard/wg0.conf $varBackupDir/$varDate/wireguard
+
+
 
 # Environment Files
 echo Backing up .env...
@@ -77,6 +82,16 @@ cp -rpi /opt/snapraid-runner/snapraid-runner.conf $varBackupDir/$varDate
 
 
 # Zip file
-echo Creating Zip...
+echo Creating Primary Zip...
 cd $varBackupDir
+du -h --max-depth=1 $varDate | sort -hr
 zip -r -9 $varDate $varDate > /dev/null 2>&1
+
+
+# Large File Storage Backups
+echo Backing Up Pinry Media
+mkdir -p $varBackupDir/$varDate-pinry
+cp -rpi $varConfigDir/pinry/static/media $varBackupDir/$varDate-pinry
+echo Creating Pinry Media Zip...
+cd $varBackupDir/$varDate-pinry
+zip -r -9 $varDate-pinry $varDate-pinry > /dev/null 2>&1
