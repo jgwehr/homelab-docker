531ba376525382909b5a18b16b41fa9cbb0df110
jgwehr <dm.nota@gmail.com>
2024-04-01 13:54:57 -0500

    Fancy progress bars
    and remove wireguard

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -45,67 +45,91 @@ cd $varOptDir/services/security && docker compose down # Endlessh, Crowdsec
 
 # Docker config backups
 echo Backing up Docker Configs...
+printf "[                  ] 0/18\r"
 
 mkdir -p $varBackupDir/$varDate/changedetection
 varTempChangeDetectionBackup=$(ls -Art $varConfigDir/changedetection/*.zip | tail -n 1)
 cp -rpi $varTempChangeDetectionBackup $varBackupDir/$varDate/changedetection
+printf "[#                 ] 1/18\r"
 
 mkdir -p $varBackupDir/$varDate/ghs && cp -rpi $varConfigDir/ghs/ghs.sqlite $varBackupDir/$varDate/ghs
+printf "[##                ] 2/18\r"
 
 mkdir -p $varBackupDir/$varDate/homepage && cp -rpi $varConfigDir/homepage/*.yaml $varBackupDir/$varDate/homepage
+printf "[###               ] 3/18\r"
 
 mkdir -p $varBackupDir/$varDate/jellyfin
 cp -rpi $varConfigDir/jellyfin/config $varBackupDir/$varDate/jellyfin
 cp -rpi $varConfigDir/jellyfin/data $varBackupDir/$varDate/jellyfin
 cp -rpi $varConfigDir/jellyfin/root $varBackupDir/$varDate/jellyfin
+printf "[####              ] 4/18\r"
 
 mkdir -p $varBackupDir/$varDate/jellyseerr
 cp -rpi $varConfigDir/jellyseerr/db $varBackupDir/$varDate/jellyseerr
 cp -rpi $varConfigDir/jellyseerr/settings.json $varBackupDir/$varDate/jellyseerr
+printf "[#####             ] 5/18\r"
 
 
 # the *arr containers smartly have scheduled backups. So we pull only the most recent to save disk space
 mkdir -p $varBackupDir/$varDate/prowlarr
 varTempProwlarrBackup=$(ls -Art $varConfigDir/prowlarr/Backups/scheduled | tail -n 1)
 cp -rpi $varConfigDir/prowlarr/Backups/scheduled/$varTempProwlarrBackup $varBackupDir/$varDate/prowlarr
+printf "[######            ] 6/18\r"
+
+mkdir -p $varBackupDir/$varDate/lidarr
+varTempRadarrBackup=$(ls -Art $varConfigDir/lidarr/Backups/scheduled | tail -n 1)
+cp -rpi $varConfigDir/lidarr/Backups/scheduled/$varTempRadarrBackup $varBackupDir/$varDate/lidarr
+printf "[#######           ] 7/18\r"
 
 mkdir -p $varBackupDir/$varDate/radarr3
 varTempRadarrBackup=$(ls -Art $varConfigDir/radarr3/Backups/scheduled | tail -n 1)
 cp -rpi $varConfigDir/radarr3/Backups/scheduled/$varTempRadarrBackup $varBackupDir/$varDate/radarr3
+printf "[########          ] 8/18\r"
 
 mkdir -p $varBackupDir/$varDate/sonarr
 varTempSonarrBackup=$(ls -Art $varConfigDir/sonarr/Backups/scheduled | tail -n 1)
 cp -rpi $varConfigDir/sonarr/Backups/scheduled/$varTempSonarrBackup $varBackupDir/$varDate/sonarr
+printf "[#########         ] 9/18\r"
 
 mkdir -p $varBackupDir/$varDate/qbittorrent
 cp -rpi $varConfigDir/qbt/qBittorrent/qBittorrent.conf $varBackupDir/$varDate/qbittorrent
+printf "[##########        ] 10/18\r"
+
+mkdir -p $varBackupDir/$varDate/gluetun
+cp -rpi $varConfigDir/gluetun/servers.json $varBackupDir/$varDate/gluetun
+printf "[###########       ] 11/18\r"
 
 mkdir -p $varBackupDir/$varDate/pihole && cp -rpi $varConfigDir/pihole $varBackupDir/$varDate
 # the below databases are very large and can be rebuilt
 rm $varBackupDir/$varDate/pihole/pihole/gravity.db
 rm $varBackupDir/$varDate/pihole/pihole/gravity_old.db
 rm $varBackupDir/$varDate/pihole/pihole/pihole-FTL.db
+printf "[############      ] 12/18\r"
 
 mkdir -p $varBackupDir/$varDate/pinry
 cp -rpi $varConfigDir/pinry/*.* $varBackupDir/$varDate/pinry
+printf "[#############     ] 13/18\r"
 
 mkdir -p $varBackupDir/$varDate/podgrab
 cp -rpi $varConfigDir/podgrab/podgrab.db $varBackupDir/$varDate/podgrab
 varTempPodgrabBackup=$(ls -Art $varConfigDir/podgrab/backups | tail -n 1)
 cp -rpi $varConfigDir/podgrab/backups/$varTempPodgrabBackup $varBackupDir/$varDate/podgrab/backups
+printf "[##############    ] 14/18\r"
 
 mkdir -p $varBackupDir/$varDate/ripping && cp -rpi $varConfigDir/ripping $varBackupDir/$varDate
+printf "[###############   ] 15/18\r"
 
 mkdir -p $varBackupDir/$varDate/scrutiny && cp -rpi $varConfigDir/scrutiny $varBackupDir/$varDate
+printf "[################  ] 16/18\r"
 
 mkdir -p $varBackupDir/$varDate/unbound && cp -rpi $varConfigDir/unbound $varBackupDir/$varDate
+printf "[################# ] 17/18\r"
 
 mkdir -p $varBackupDir/$varDate/uptime-kuma
 cp -rpi $varConfigDir/uptime-kuma/kuma.db $varBackupDir/$varDate/uptime-kuma
+printf "[##################] 18/18\r"
 
-mkdir -p $varBackupDir/$varDate/wireguard
-cp -rpi $varConfigDir/wireguard/wg0.conf $varBackupDir/$varDate/wireguard
-
+printf "\n"
 
 
 # Environment Files
